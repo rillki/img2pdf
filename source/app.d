64e1f45c8d0d778b230c74b72a26dc48076bc114
Kirill Saidov <kirillsaidovd@yahoo.com>
2021-09-21 21:53:15 +0600

    toPDF func added

--- source/app.d
+++ source/app.d
@@ -1,6 +1,31 @@
-import std.stdio;
+module app;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import std.file: write;	
+
+import printed.canvas;
+
+void main(string[] args) {
+	"assets/output2.pdf".toPDF(["assets/cat.jpg"]);
+}
+
+void toPDF(const string filename, const string[] imgs) {
+	import std.file: write;
+	import printed.canvas: PDFDocument, IRenderingContext2D;
+	
+	auto pdf = new PDFDocument();
+	auto context = cast(IRenderingContext2D)(pdf);
+	
+	// print images to pdf
+	foreach(img; imgs) {
+		// open image
+		auto currentImg = new Image(img);
+		
+		// print image to pdf
+		context.drawImage(currentImg, 0, 0, context.pageWidth, context.pageHeight);
+		
+		// create new page
+		context.newPage();
+	}
+	
+	filename.write(pdf.bytes);
 }
