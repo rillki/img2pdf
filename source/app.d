8eef382475c59ab6cd66d8fa9c77bd0de6490c0b
Kirill Saidov <kirillsaidovd@yahoo.com>
2021-09-22 07:12:44 +0600

    building command line interface

--- source/app.d
+++ source/app.d
@@ -1,16 +1,26 @@
 module app;
 
-import std.file: write;	
-
-import printed.canvas;
+import std;
 
 void main(string[] args) {
-	"assets/output2.pdf".toPDF(["assets/cat.jpg"]);
+	if(args.length < 2) {
+		("\n========================================================\n" ~
+			"img2pdf  v1.0 -- Image to PDF converter.\n" ~
+			"--------------------------------------------------------\n" ~
+			"USAGE:\n\timg2pdf [path] [imgs,coma,seperated] [file.pdf]\n" ~
+			"DEFAULTS:\n\tpath\tcwd\n\timgs\tall files in path\n\tfile\tpath/output.pdf\n" ~
+			"EXAMPLE:\n\timg2pdf ../temp/ img1.png,img2.jpg myImages.pdf\n" ~
+			"========================================================\n"
+		).writeln;
+		return;
+	}
+	
+	// find path,pdfname,imgs
 }
 
-void toPDF(const string filename, const string[] imgs) {
+void toPDF(const string pdfName, const string[] imgs) {
 	import std.file: write;
-	import printed.canvas: PDFDocument, IRenderingContext2D;
+	import printed.canvas: PDFDocument, IRenderingContext2D, Image;
 	
 	auto pdf = new PDFDocument();
 	auto context = cast(IRenderingContext2D)(pdf);
@@ -27,5 +37,5 @@ void toPDF(const string filename, const string[] imgs) {
 		context.newPage();
 	}
 	
-	filename.write(pdf.bytes);
+	pdfName.write(pdf.bytes);
 }
