87a856287daf24e51cc6d490e77560c2a389ec27
kirill.saidov <kirill.saidov.d@gmail.com>
2021-09-29 20:53:56 +0600

    minor changes

--- source/app.d
+++ source/app.d
@@ -1,7 +1,19 @@
 module app;
 
-import std;
-import pdfed;
+// phobos
+import std.stdio: writefln;
+import std.array: array, empty;
+import std.file: write, exists, getcwd, dirEntries, SpanMode;
+import std.path: dirSeparator, buildPath, baseName;
+import std.conv: to;
+import std.algorithm: splitter, filter, map, canFind, sort, reverse;
+
+// printed
+import printed.canvas: PDFDocument, IRenderingContext2D, Image;
+
+// pdf document dimensions
+immutable pageWidthmm = 210.0;
+immutable pageHeightmm = 297.0;
 
 void main(string[] args) {
 	if(args.length < 2) {
@@ -26,7 +38,7 @@ void main(string[] args) {
 			"\nEXAMPLE:\n\timg2pdf ../temp/ img1.png,img2.jpg myImages.pdf\n" ~
 			"\t\t-sfalse -asc -portrait\n" ~
 			"=========================================================\n"
-		).writeln;
+		).writefln;
 		return;
 	}
 
@@ -99,6 +111,67 @@ string[] listdir(const string dir) {
         .map!(a => baseName(a.name))
         .array;
 }
+
+/++
+Converts images to a PDF file
+
+Params:
+	pdfDocumentName = pdf file name
+	images = an array image names including the path
+	stretchToPDFSize = stretches image to pdf page width and height, `true` by default 
+	orientPortrait = portrait page orientation, `true` by default
++/
+void img2pdf(const string pdfDocumentName, const string[] images, const bool stretchToPDFSize = true, const bool orientPortrait = true) {	
+	// create a pdf document
+	PDFDocument pdf;
+	if(orientPortrait) {
+		pdf = new PDFDocument(pageWidthmm, pageHeightmm);
+	} else {
+		pdf = new PDFDocument(pageHeightmm, pageWidthmm);
+	}
+	auto context = cast(IRenderingContext2D)(pdf);
+	
+	// prints images to pdf
+	foreach(i, img; images) {
+		// creates a new page
+		if(i > 0) {
+			context.newPage();
+		}
+
+		// checks if image exists
+		if(!img.exists) { 
+			writefln("#img2pdf: (%s) <%s> does not exist! Skipping...", i, img);
+			continue; 
+		}
+
+		// opens an image (skips the image upon failure)
+		Image currentImg;
+		try {
+			currentImg = new Image(img);
+		} catch(Exception e) {
+			writefln("#img2pdf: (%s) <%s> failed to open; %s", i, img, e.msg);
+			continue;
+		}
+		
+		// prints an image to a blank pdf page
+		if(stretchToPDFSize) {
+			context.drawImage(currentImg, 0, 0, context.pageWidth, context.pageHeight);
+		} else {
+			context.drawImage(currentImg, 0, 0);
+		}
+		
+		writefln("#img2pdf: (%s) <%s> converted!", i, img.splitter(dirSeparator).array[$-1]);
+	}
+	
+	// writes data to pdf file
+	writefln("#img2pdf: saving <%s>", pdfDocumentName);
+	try {
+		pdfDocumentName.write(pdf.bytes);
+	} catch(Exception e) {
+		writefln("#img2pdf: failed to save <%s>; %s", pdfDocumentName, e.msg);		
+	}
+}
+
 
 
 
